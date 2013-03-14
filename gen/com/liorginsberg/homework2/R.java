a08104005904002d4ce55ee4343029137443285e
liorgins <lginsberg9a@gamil.com>
2013-03-14 03:46:23 +0200

    Finish froyo and jellybean

--- gen/com/liorginsberg/homework2/R.java
+++ gen/com/liorginsberg/homework2/R.java
@@ -23,21 +23,20 @@ public final class R {
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
         public static final int marker=0x7f020001;
-        public static final int shenkar_image=0x7f020002;
-        public static final int tick=0x7f020003;
+        public static final int marker_select=0x7f020002;
+        public static final int shenkar_image=0x7f020003;
+        public static final int shenkar_image_land=0x7f020004;
+        public static final int tick=0x7f020005;
     }
     public static final class id {
         public static final int background=0x7f070000;
         public static final int etText=0x7f070003;
-        public static final int markerImage=0x7f070006;
-        public static final int markerWrapper=0x7f070005;
         public static final int overlay=0x7f070001;
         public static final int save=0x7f070004;
         public static final int textPanel=0x7f070002;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
-        public static final int marker_view=0x7f030001;
     }
     public static final class string {
         public static final int app_name=0x7f050000;
--- src/com/liorginsberg/homework2/MainActivity.java
+++ src/com/liorginsberg/homework2/MainActivity.java
@@ -1,10 +1,10 @@
 package com.liorginsberg.homework2;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.view.GestureDetectorCompat;
 import android.util.DisplayMetrics;
-import android.util.Log;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.View;
@@ -12,8 +12,11 @@ import android.view.View.OnClickListener;
 import android.view.ViewTreeObserver.OnGlobalLayoutListener;
 import android.view.Window;
 import android.view.animation.Animation;
+import android.view.animation.AnimationSet;
 import android.view.animation.BounceInterpolator;
+import android.view.animation.ScaleAnimation;
 import android.view.animation.TranslateAnimation;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
@@ -22,15 +25,9 @@ import android.widget.RelativeLayout;
 
 public class MainActivity extends Activity {
 
-	
-
-	public static final int REQUEST_EDIT_MARKER = 1001;
-
 	private RelativeLayout overlay;
 	private GestureDetectorCompat mDetector;
 	private int offsetY = 0;
-
-
 
 	private LinearLayout textPanel;
 	private TranslateAnimation slideUp;
@@ -38,17 +35,57 @@ public class MainActivity extends Activity {
 
 	private ImageButton saveButton;
 
-
 	private EditText etText;
+	
+	Animation.AnimationListener collapseListener = new Animation.AnimationListener() {
+		
+		public void onAnimationEnd(Animation animation) {
+			textPanel.setVisibility(View.GONE);
+		}
 
+		public void onAnimationRepeat(Animation animation) {
+		
+		}
+
+		public void onAnimationStart(Animation animation) {
+		
+		}
+	};
+	
+	OnClickListener myClickListener = new OnClickListener() {
+
+		@Override
+		public void onClick(View v) {
+			ImageView marker = (ImageView) v;
+			ImageView lastMarker = (ImageView) textPanel.getTag();
+			if (lastMarker != null) {
+				lastMarker.setImageResource(R.drawable.marker);
+			}
+			marker.setImageResource(R.drawable.marker_select);
+			textPanel.setVisibility(View.VISIBLE);
+			textPanel.setTag(v);
+			String fromMark = (String) v.getTag();
+			if (fromMark != null) {
+				etText.setText(fromMark);
+			} else {
+				etText.setText("");
+			}
+
+			textPanel.startAnimation(slideDown);
+		}
+	};
+	
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.activity_main);
 
+		overlay = (RelativeLayout) findViewById(R.id.overlay);
+
 		textPanel = (LinearLayout) findViewById(R.id.textPanel);
-		etText = (EditText)findViewById(R.id.etText);
+		etText = (EditText) findViewById(R.id.etText);
 		slideUp = new TranslateAnimation(0.0f, 0.0f, 0.0f,
 				DisplayHelper.convertDpToPixel(-40, this));
 		slideUp.setDuration(750);
@@ -56,23 +93,22 @@ public class MainActivity extends Activity {
 		slideDown = new TranslateAnimation(0.0f, 0.0f,
 				DisplayHelper.convertDpToPixel(-40, this), 0.0f);
 		slideDown.setDuration(750);
-		
 
 		saveButton = (ImageButton) findViewById(R.id.save);
 		saveButton.setOnClickListener(new OnClickListener() {
 
 			@Override
 			public void onClick(View v) {
-				View mark = (View)textPanel.getTag();
-				mark.setTag(etText.getText().toString());
+				ImageView marker = (ImageView) textPanel.getTag();
+				marker.setImageResource(R.drawable.marker);
+				marker.setTag(etText.getText().toString());
 				textPanel.startAnimation(slideUp);
+				InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+				imm.hideSoftInputFromWindow(etText.getWindowToken(), 0);
 
 			}
 		});
 
-		
-
-		overlay = (RelativeLayout) findViewById(R.id.overlay);
 
 		final OnGlobalLayoutListener offsetYLayoutListener = new OnGlobalLayoutListener() {
 
@@ -97,19 +133,6 @@ public class MainActivity extends Activity {
 		mDetector = new GestureDetectorCompat(this, new MyGestureListener());
 	}
 
-	Animation.AnimationListener collapseListener = new Animation.AnimationListener() {
-		public void onAnimationEnd(Animation animation) {
-			textPanel.setVisibility(View.GONE);
-		}
-
-		public void onAnimationRepeat(Animation animation) {
-			// not needed
-		}
-
-		public void onAnimationStart(Animation animation) {
-			// not needed
-		}
-	};
 
 	@Override
 	public boolean onTouchEvent(MotionEvent event) {
@@ -117,42 +140,22 @@ public class MainActivity extends Activity {
 	}
 
 	class MyGestureListener extends GestureDetector.SimpleOnGestureListener {
-		private static final String DEBUG_TAG = "Gestures";
-
-		OnClickListener myClickListener = new OnClickListener() {
-
-			@Override
-			public void onClick(View v) {
-				textPanel.setVisibility(View.VISIBLE);
-				textPanel.setTag(v);
-				String fromMark = (String)v.getTag();
-				if(fromMark != null) {
-					etText.setText(fromMark);
-				} else {
-					etText.setText("");
-				}
-				
-				textPanel.startAnimation(slideDown);				
-			}
-
-		};
-
+		
 		@Override
 		public boolean onDown(MotionEvent event) {
-			// Get the Y OFfset
-
-			Log.d(DEBUG_TAG, "x: " + event.getX() + " y: "
-					+ (event.getY() - offsetY));
 			return true;
 		}
 
 		@Override
 		public void onLongPress(MotionEvent event) {
-			
-			int finalYPosition = (int) (Math.floor(event.getY() - offsetY - DisplayHelper.convertDpToPixel(56, getApplicationContext())));
-			
+
+			int finalYPosition = (int) (Math.floor(event.getY()
+					- offsetY
+					- DisplayHelper.convertDpToPixel(56,
+							getApplicationContext())));
+
 			ImageView image = new ImageView(MainActivity.this);
-			
+
 			RelativeLayout.LayoutParams vp = new RelativeLayout.LayoutParams(
 					(int) DisplayHelper.convertDpToPixel(56, MainActivity.this),
 					(int) DisplayHelper.convertDpToPixel(56, MainActivity.this));
@@ -160,22 +163,29 @@ public class MainActivity extends Activity {
 			vp.setMargins(
 					(int) (Math.floor(event.getX()
 							- DisplayHelper.convertDpToPixel(28,
-									getApplicationContext()))),finalYPosition, 0, 0);
-			
+									getApplicationContext()))), finalYPosition,
+					0, 0);
+
 			image.setLayoutParams(vp);
 
 			image.setImageResource(R.drawable.marker);
 
-			
 			image.setOnClickListener(myClickListener);
 
-			TranslateAnimation bounce_marker = new TranslateAnimation(0.0f,
-																	  0.0f,
-																	  (DisplayHelper.convertDpToPixel(-finalYPosition, getApplicationContext())),
-																	   0.0f);
-			bounce_marker.setInterpolator(new BounceInterpolator());
-			bounce_marker.setDuration(1000);
-			image.startAnimation(bounce_marker);
+			AnimationSet bounceMarkerAnimationSet = new AnimationSet(true);
+			bounceMarkerAnimationSet.setInterpolator(new BounceInterpolator());
+			bounceMarkerAnimationSet.setDuration(1000);
+			TranslateAnimation trans = new TranslateAnimation(0.0f, 0.0f,
+					(DisplayHelper.convertDpToPixel(-finalYPosition,
+							getApplicationContext())), 0.0f);
+			ScaleAnimation scale = new ScaleAnimation(4, 1, 4, 1,
+					Animation.RELATIVE_TO_SELF, 0.5f,
+					Animation.RELATIVE_TO_SELF, 0.5f);
+			bounceMarkerAnimationSet.addAnimation(trans);
+			bounceMarkerAnimationSet.addAnimation(scale);
+
+			image.startAnimation(bounceMarkerAnimationSet);
+			
 			overlay.addView(image);
 			super.onLongPress(event);
 		}
@@ -183,8 +193,7 @@ public class MainActivity extends Activity {
 		@Override
 		public boolean onFling(MotionEvent event1, MotionEvent event2,
 				float velocityX, float velocityY) {
-			Log.d(DEBUG_TAG,
-					"onFling: " + event1.toString() + event2.toString());
+			
 			return true;
 		}
 	}
